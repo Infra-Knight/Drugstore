552b2c9888d6b3a8d41a002553d0eef0af15fcca
Nhan Doan <nhandev88@gmail.com>
2018-11-23 00:33:35 +0700

    Update database
    - Correct fields’ data type
    
    - Change collation to Unicode

--- clc2015.sql
+++ clc2015.sql
@@ -2,13 +2,21 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 21, 2018 at 09:56 PM
--- Server version: 5.7.23
--- PHP Version: 5.6.37
+-- Host: localhost
+-- Generation Time: Nov 22, 2018 at 05:30 PM
+-- Server version: 8.0.12
+-- PHP Version: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `clc2015`
@@ -22,30 +30,30 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `drugs` (
   `id` int(11) NOT NULL,
-  `name` varchar(500) NOT NULL,
-  `category` int(11) NOT NULL,
-  `price` varchar(500) NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `image` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` float NOT NULL DEFAULT '0',
+  `description` varchar(8192) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `drugs`
 --
 
 INSERT INTO `drugs` (`id`, `name`, `category`, `price`, `description`, `image`) VALUES
-(1, 'a a', 111, 'a@mail', '', '/images/ahihi.png'),
-(2, 'b a', 222, 'b@mail', '', '/images/hello.png'),
-(3, 'c a', 333, 'c@mail', '', NULL),
-(4, 'a d', 444, 'd@mail', '', NULL),
-(5, 'a e', 555, 'e@mail', '', NULL),
-(6, 'f a', 666, 'f@mail', '', NULL),
-(7, 'g b', 777, 'g@mail', '', NULL),
-(8, 'h', 888, 'h@mail', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a natus nobis. Molestiae, eligendi modi? Alias accusantium eius quisquam eos, aut necessitatibus dolorem debitis dignissimos maxime facilis nam omnis quibusdam.', NULL),
-(9, 'i', 999, 'i@mail', '', NULL),
-(10, 'k', 101, 'k@mail', '', NULL),
-(11, 'James.B', 7, 'j@agent.com', '', NULL),
-(1552436, 'Hien Vinh', 1234, 'vinh@mail.com', '', NULL);
+(1, 'a a', '111', 0, '', '/images/ahihi.png'),
+(2, 'b a', '222', 5, '', '/images/hello.png'),
+(3, 'Thuốc', '333', 10, '', NULL),
+(4, 'CLGTN', '444', 1, '', NULL),
+(5, 'Cỏ', '555', 0, '', NULL),
+(6, 'Cần', '666', 0, '', NULL),
+(7, 'Đoàn Tô', '777', 50, '', NULL),
+(8, 'h', '888', 16.5, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a natus nobis. Molestiae, eligendi modi? Alias accusantium eius quisquam eos, aut necessitatibus dolorem debitis dignissimos maxime facilis nam omnis quibusdam.', NULL),
+(9, 'i', '999', 61.35, '', NULL),
+(10, 'k', '101', 2.99, '', NULL),
+(11, 'James.B', '7', 6.75, '', NULL),
+(12, 'Hiển', '1234', 0, '', NULL);
 
 -- --------------------------------------------------------
 
@@ -54,18 +62,19 @@ INSERT INTO `drugs` (`id`, `name`, `category`, `price`, `description`, `image`) 
 --
 
 CREATE TABLE `user` (
-  `fullname` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `fullname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`fullname`, `email`, `password`) VALUES
-('vinh', 'c@mail.com', 'c12345678'),
-('hvinh', 'a@mail.com', 'a12345678');
+INSERT INTO `user` (`id`, `fullname`, `email`, `password`) VALUES
+(1, 'vinh', 'c@mail.com', 'c12345678'),
+(2, 'hvinh', 'a@mail.com', 'a12345678');
 
 --
 -- Indexes for dumped tables
@@ -77,3 +86,31 @@ INSERT INTO `user` (`fullname`, `email`, `password`) VALUES
 ALTER TABLE `drugs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `drugs`
+--
+ALTER TABLE `drugs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
